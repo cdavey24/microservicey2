5abf37c8957306dc18c257854da4394ca917cd0d
Colm Davey <colm.davey@atu.ie>
2026-03-17 13:35:07 +0000

    Added API Gateway endpoint

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function uploadFile() {
 
     const file = document.getElementById("fileInput").files[0];
 
-    const response = await fetch("YOUR_API_ENDPOINT");
+    const response = await fetch("https://cg7fr2nkgl.execute-api.eu-west-1.amazonaws.com/dev/upload-url");
 
     const data = await response.json();
 
